315e637194e1345d1e3917125b36cd170e294446
qiuguanbin <ben233q@gmail.com>
2019-09-08 20:45:12 +0800

    异步加载蜜汁不生效

--- src/routes/main/home/menuRoute.tsx
+++ src/routes/main/home/menuRoute.tsx
@@ -2,8 +2,10 @@ import React from "react";
 import AsynLoadable from "components/AsynLoadable";
 import { Route, Switch, Redirect } from "react-router-dom";
 
-const Dashboard = AsynLoadable(() => import(/* webpackChunkName: "dashboard" */ "routes/main/dashboard"));
-const User = AsynLoadable(() => import(/* webpackChunkName: "user" */ "routes/main/user"));
+import Dashboard from "routes/main/dashboard";
+import User from "routes/main/user";
+// const Dashboard = AsynLoadable(() => import(/* webpackChunkName: "dashboard" */ "routes/main/dashboard"));
+// const User = AsynLoadable(() => import(/* webpackChunkName: "user" */ "routes/main/user"));
 
 export const MenuRoute = () => {
     return (
